65308eab1f95599615460a7c1dcfce429681fba9
sebasrh <sebastian.hidalgo@correounivalle.edu.co>
2022-12-17 23:34:47 -0500

    Mas o menos

--- Apuesta/Aplicaciones/Partidos/models.py
+++ Apuesta/Aplicaciones/Partidos/models.py
@@ -4,29 +4,26 @@ from django.db import models
 # Create your models here.
 
 class Equipo(models.Model):
-    pais = models.CharField(primary_key=True,max_length=50)
+    pais = models.CharField(max_length=50)
     grupo = models.CharField(max_length=50)
 
     def __str__(self):
         texto = "{0} ({1})"
         return texto.format(self.pais, self.grupo)
 
-
 class Partido(models.Model):
-    codigo = models.CharField(primary_key=True,max_length=6)
     clasificacion = models.CharField(max_length=50)
-    equipos1 = models.ForeignKey(Equipo,blank=True,null=True,on_delete=models.CASCADE,related_name="%(class)s_equipos1")
-    equipos2 = models.ForeignKey(Equipo,blank=True,null=True,on_delete=models.CASCADE,related_name="%(class)s_equipos2")
+    equipos1 = models.ForeignKey(Equipo,blank=False,null=False,on_delete=models.CASCADE,related_name="_equipos1")
+    equipos2 = models.ForeignKey(Equipo,blank=False,null=False,on_delete=models.CASCADE,related_name="_equipos2")
     marcador1 = models.CharField(max_length=2)
     marcador2 = models.CharField(max_length=2)
     fecha = models.DateField()
 
     def __str__(self):
         texto = "{0} ({1})"
-        return texto.format(self.nombre, self.codigo)
+        return texto.format(self.clasificacion, self.codigo)
 
 class Apuesta(models.Model):
-    codigo_apuesta = models.CharField(primary_key=True,max_length=6)
     codigo_partido = models.ForeignKey(Partido,blank=True,null=True,on_delete=models.CASCADE)
     nombre = models.CharField(max_length=50)
     marcador1 = models.CharField(max_length=2)
@@ -35,4 +32,4 @@ class Apuesta(models.Model):
 
     def __str__(self):
         texto = "{0} ({1})"
-        return texto.format(self.nombre, self.codigo)
+        return texto.format(self.nombre, self.monto)
